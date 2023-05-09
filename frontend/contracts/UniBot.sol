49d21e48a858ebb97de368de0a1cf099cb973d01
Sebas <sheva3232@gmail.com>
2023-05-08 20:43:00 -0500

    proposal logic contract v0.1

--- frontend/contracts/UniBot.sol
+++ frontend/contracts/UniBot.sol
@@ -4,6 +4,7 @@ pragma abicoder v2;
 
 import "https://github.com/Uniswap/v3-periphery/blob/main/contracts/interfaces/ISwapRouter.sol";
 
+
 interface IERC20 {
     function balanceOf(address account) external view returns (uint256);
 
@@ -15,6 +16,19 @@ interface IERC20 {
 }
 
 contract SingleSwap {
+    //logic data
+    address private owner;
+    struct GridSetting {
+        uint256 balanceStable;
+        uint256 balanceTarget;
+        uint256[] targets;
+        uint256 lastTarget;
+    }
+    //mapping storaging the user address, inside nested, the tx ID for that user related to a GridSetting
+    mapping (address => mapping(uint256 => GridSetting)) gridsState;
+
+
+    //swap data
     address public constant routerAddress =
         0xE592427A0AEce92De3Edee1F18E0157C05861564;
     ISwapRouter public immutable swapRouter = ISwapRouter(routerAddress);
@@ -27,8 +41,40 @@ contract SingleSwap {
     // For this example, we will set the pool fee to 0.3%.
     uint24 public constant poolFee = 3000;
 
-    constructor() {}
 
+    constructor() {
+        owner = msg.sender; //or we can use openzeppeling onlyowner library
+    }
+    //logic functions
+    function callToAction(uint256 _currentPrice, address _swapOnwer, uint256 _swapOrder) public {
+        require(msg.sender == owner, 'Only owner can call this function');
+        GridSetting memory grid = gridsState[_swapOnwer][_swapOrder];
+        //if new price is higher --- must sell
+        if(_currentPrice > grid.lastTarget){
+            //TOdo call sell function
+            gridsState[_swapOnwer][_swapOrder].lastTarget = _currentPrice;
+            //TOdo adjust balance for Stable and Target tokens;
+        }
+        //if new price is lower -- must purchase new tokens
+        else {
+            //TOdo call purchase function
+            gridsState[_swapOnwer][_swapOrder].lastTarget = _currentPrice;
+            //TOdo adjust balance for Stable and Target tokens;
+        }
+    }
+    function readLastPos (address _swapOnwer, uint256 _swapOrder) public view returns (uint256) {
+        return gridsState[_swapOnwer][_swapOrder].lastTarget;
+    }
+    function getStableBalance (address _swapOnwer, uint256 _swapOrder) public view returns (uint256) {
+        return gridsState[_swapOnwer][_swapOrder].balanceStable;
+    }
+    function getTargetBalance (address _swapOnwer, uint256 _swapOrder) public view returns (uint256) {
+        return gridsState[_swapOnwer][_swapOrder].balanceTarget;
+    }
+    function getTargets (address _swapOnwer, uint256 _swapOrder) public view returns (uint256[] memory) {
+        return gridsState[_swapOnwer][_swapOrder].targets;
+    }
+    //swap functions
     function swapExactInputSingle(uint256 amountIn)
         external
         returns (uint256 amountOut)
@@ -75,5 +121,5 @@ contract SingleSwap {
             linkToken.transfer(address(this), amountInMaximum - amountIn);
         }
     }
-    
+
 }

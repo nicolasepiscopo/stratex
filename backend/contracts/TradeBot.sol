57313ecff2cdd7e12e824aea740aa14c03ea05dc
sivaramaaa <sivaramaaa@gmail.com>
2023-05-18 22:27:55 +0530

    Trade Bot V1

--- backend/contracts/TradeBot.sol
+++ backend/contracts/TradeBot.sol
@@ -20,7 +20,6 @@ interface IERC20 {
 
 contract SingleSwap is AutomationCompatibleInterface {
 
-    uint256 public counter;
     AggregatorV3Interface internal priceFeed;
     address public constant routerAddress = 0xE592427A0AEce92De3Edee1F18E0157C05861564;
     ISwapRouter public immutable swapRouter = ISwapRouter(routerAddress);
@@ -28,11 +27,18 @@ contract SingleSwap is AutomationCompatibleInterface {
     uint256[] public grids;
     mapping(uint256 => bool) private breachedGrids;
     uint256 public breachCounter;
+    uint256 public buyCounter;
+    uint256 public sellCounter;
+    mapping(uint256 => uint256) boughtAmounts;
+    uint256 private lastExecutionTime;
+    uint256 private interval = 60;
 
     address public constant WMATIC = 0x9c3C9283D3e44854697Cd22D3Faa240Cfb032889; // Polygon Network :
     address public constant WETH = 0xA6FA4fB5f76172d178d61B04b0ecd319C5d1C0aa; // Polygon Network
 
-    event GridBreached(uint256 gridIndex, bool isFirstBreach, uint256 price);
+    event GridBreached(uint256 gridIndex, bool isFirstBreach, uint256 qty ,uint256 price);
+    event BytesFailure(bytes bytesFailure);
+    event StringFailure(string stringFailure);
 
 
     //address public constant GETH  = 0xdD69DB25F6D620A7baD3023c5d32761D353D3De9 ; // Goerli ETH
@@ -48,50 +54,80 @@ contract SingleSwap is AutomationCompatibleInterface {
         priceFeed = AggregatorV3Interface(
             0x0715A7794a1dc8e42615F059dD6e406A6594651A // ETH-USD in Mumbai Testnet
         );
-        counter = 1337;
-        grids = [1787, 1796, 1805, 1814, 1823, 1832, 1842];
+        grids = [1787, 1796, 1805, 1814, 1823, 1832, 1842, 1862];
         breachCounter = 0;
     }
 
     function checkUpkeep(bytes calldata ) external view override returns (bool upkeepNeeded, bytes memory performData)
     {
         uint price = getScaledPrice();
+        uint256 minDistance = type(uint256).max;
+        uint256 breachIndex = grids.length;
         for (uint256 i = 0; i < grids.length; i++) {
-            if (price > grids[i] && !breachedGrids[i]) {
+            uint256 distance = price > grids[i] ? price - grids[i] : grids[i] - price;
+            if (distance < minDistance) {
+                minDistance = distance;
+                breachIndex = i;
+            }
+        }
+        bool telapsed = (block.timestamp - lastExecutionTime) > interval;
+        if (minDistance <= 2 && telapsed) { // In vicinity of 0.10% of the Grid
+            if (!breachedGrids[breachIndex]) {
                 upkeepNeeded = true;
-                performData = abi.encode(i, true);
-                break;
-            } else if (price < grids[i] && breachedGrids[i]) {
+                performData = abi.encode(breachIndex, true);
+            } else {
                 upkeepNeeded = true;
-                performData = abi.encode(i, false);
-                break;
+                performData = abi.encode(breachIndex, false);
             }
         }
     }
 
     function performUpkeep(bytes calldata performData) external override {
-        counter += 1;
-        (uint256 i, bool isFirstBreach) = abi.decode(performData, (uint256, bool));
-        if (isFirstBreach) {
-            breachedGrids[i] = true;
-            breachCounter++;
-            emit GridBreached(i, true , getScaledPrice());
+        uint256 amountToSell = 0;
+        (uint256 breachedIndex, bool isFirstBreach) = abi.decode(performData, (uint256, bool));
+        if (isFirstBreach) { // This needs to fixed maybe add same sell code in else part 
+            breachedGrids[breachedIndex] = true;
+            breachCounter++; 
+            // Perform Buy if there is no Order placed in n-1 grid , else Sell
+            if (breachedIndex > 0 && !breachedGrids[breachedIndex - 1]) {
+                buyCounter++;
+                uint256 qty = swapExactInputSingle(150000000000000000, WMATIC, WETH);
+                boughtAmounts[breachedIndex] = qty; // This quantity will come from exactInputSingle() call
+                emit GridBreached(breachedIndex, true , qty, getScaledPrice());
+            } else {
+                amountToSell = boughtAmounts[breachedIndex - 1];
+                if (amountToSell > 0) {
+                    delete boughtAmounts[breachedIndex - 1];
+                    delete breachedGrids[breachedIndex - 1];
+                    sellCounter++;
+                    swapExactInputSingle(amountToSell, WETH, WMATIC);
+                    emit GridBreached(breachedIndex, false, amountToSell, getScaledPrice());
+                }
+            }
         } else {
-            breachedGrids[i] = false;
-            breachCounter--;
-            emit GridBreached(i, false, getScaledPrice());
-        }
+            if (breachedGrids[breachedIndex - 1]){
+                amountToSell = boughtAmounts[breachedIndex - 1];
+                if (amountToSell > 0) {
+                    delete boughtAmounts[breachedIndex - 1];
+                    delete breachedGrids[breachedIndex - 1];
+                    sellCounter++;
+                    swapExactInputSingle(amountToSell, WETH, WMATIC);
+                    emit GridBreached(breachedIndex, false, amountToSell, getScaledPrice());
+                }
+            }
+       }
+       lastExecutionTime = block.timestamp;
     }
 
 
-    function swapExactInputSingle(uint256 amountIn) external payable returns (uint256 amountOut)
+    function swapExactInputSingle(uint256 amountIn, address tin , address tout) public payable returns (uint256 amountOut)
     {
         linkToken.approve(address(swapRouter), amountIn);
 
         ISwapRouter.ExactInputSingleParams memory params = ISwapRouter
             .ExactInputSingleParams({
-                tokenIn:  WMATIC,
-                tokenOut: WETH,
+                tokenIn:  tin,
+                tokenOut: tout,
                 fee: poolFee,
                 recipient: address(this),
                 deadline: block.timestamp,
@@ -100,7 +136,16 @@ contract SingleSwap is AutomationCompatibleInterface {
                 sqrtPriceLimitX96: 0
             });
 
-       amountOut = swapRouter.exactInputSingle(params);
+       //amountOut = swapRouter.exactInputSingle(params);
+       try swapRouter.exactInputSingle(params) returns (uint256 _value) {
+           // Successful execution, proceed with the rest of the code
+           return (_value);
+       } catch Error(string memory _err) {
+           emit StringFailure(_err);
+       }
+       catch (bytes memory error) {
+           emit BytesFailure(error);
+       }
     }
 
     function withdraw() public  {

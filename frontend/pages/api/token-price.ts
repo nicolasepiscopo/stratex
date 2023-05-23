51e33889d65a7d44d014142b7b049906e120e965
Nicolás Daniel Epíscopo <nicolasepiscopo@Nicolass-MacBook-Pro.local>
2023-05-23 10:20:36 -0300

    add error response and logging for price endpoint

--- frontend/pages/api/token-price.ts
+++ frontend/pages/api/token-price.ts
@@ -3,8 +3,13 @@
  */
 export default async function handler(req, res) {
   const { symbolFrom, symbolTo = 'USD' } = JSON.parse(req.body);
-  const response = await fetch(`https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?symbol=${symbolFrom}&convert=${symbolTo}&CMC_PRO_API_KEY=${process.env.CMC_PRO_API_KEY}`);
-  const data = await response.json();
-
-  res.status(200).json(data);
+  try {
+    const response = await fetch(`https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?symbol=${symbolFrom}&convert=${symbolTo}&CMC_PRO_API_KEY=${process.env.CMC_PRO_API_KEY}`);
+    const data = await response.json();
+  
+    res.status(200).json(data);
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({ error: e.message });
+  }
 }

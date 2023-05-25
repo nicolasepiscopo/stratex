551bc212b2a67fbd1091df55efad8a67fa85d2d8
pablo_cosio <pablo.cosio@i2cat.net>
2023-05-25 21:42:48 +0200

    First version of multiple bots & users

--- backend/contracts/TradeBot.sol
+++ backend/contracts/TradeBot.sol
@@ -24,13 +24,35 @@ contract SingleSwap is AutomationCompatibleInterface {
     address public constant routerAddress = 0xE592427A0AEce92De3Edee1F18E0157C05861564;
     ISwapRouter public immutable swapRouter = ISwapRouter(routerAddress);
 
-    uint256[] public grids;
-    mapping(uint256 => bool) private breachedGrids;
-    uint256 public breachCounter;
-    uint256 public buyCounter;
-    uint256 public sellCounter;
-    mapping(uint256 => uint256) boughtAmounts;
-    uint256 private lastExecutionTime;
+    
+    struct Bot {
+        address owner;
+        uint256[] grids;
+        mapping(uint256 => bool) breachedGrids; // breachIndex => true/false
+        uint256 breachCounter;
+        bool isFirstBreach;
+        uint256 buyCounter;
+        uint256 sellCounter;
+        mapping(uint256 => uint256) boughtAmounts; // breachIndex => amount
+        uint256 lastExecutionTime;
+    }
+
+    struct User {
+        bool onboarded;
+        uint256 balanceWETH;
+        uint256 balanceWMATIC;
+    }
+
+    struct PerformData {
+        uint256 botId;
+        uint256 breachIndex;
+        bool isFirstBreach;
+    }
+
+    mapping(uint256 => Bot) bots; // uint256 woud be the botId
+    mapping(address => User) public userData;  // address is the user address
+    uint256 public botsCount = 0;
+    
     uint256 private interval = 60;
 
     address public constant WMATIC = 0x9c3C9283D3e44854697Cd22D3Faa240Cfb032889; // Polygon Network :
@@ -54,69 +76,121 @@ contract SingleSwap is AutomationCompatibleInterface {
         priceFeed = AggregatorV3Interface(
             0x0715A7794a1dc8e42615F059dD6e406A6594651A // ETH-USD in Mumbai Testnet
         );
-        grids = [1787, 1796, 1805, 1814, 1823, 1832, 1842, 1862];
-        breachCounter = 0;
+        // grids = [1787, 1796, 1805, 1814, 1823, 1832, 1842, 1862];
+        // breachCounter = 0;
     }
 
-    function checkUpkeep(bytes calldata ) external view override returns (bool upkeepNeeded, bytes memory performData)
+    function createBot(uint256[] calldata _grids) public {
+        require(
+            userData[msg.sender].onboarded &&
+            userData[msg.sender].balanceWMATIC > 0,
+            "Insufficient balance"
+        );
+        Bot storage bot = bots[botsCount];
+        bot.owner = msg.sender;
+        bot.grids = _grids;
+        botsCount += 1;
+    }
+
+    function checkUpkeep(bytes calldata /* checkData */) external view override returns (bool upkeepNeeded, bytes memory performData)
     {
         uint price = getScaledPrice();
+        PerformData[] memory performDataUnencoded; // for each botId related the PerfomData
+        upkeepNeeded = false;
+
+        for (uint256 i = 0; i < botsCount; i++) {
+            if (bots[i].grids.length > 0) {
+                (bool _upkeepNeeded, uint256 breachIndex, bool isFirstBreach) = checkBots(i, price);
+                if (_upkeepNeeded) {
+                    performDataUnencoded[i] = PerformData(i, breachIndex, isFirstBreach);
+                    upkeepNeeded = true;
+                }
+            }
+        }
+        if (upkeepNeeded) performData = abi.encode(performDataUnencoded, true);
+        return (upkeepNeeded, performData); 
+    }
+
+    function checkBots(uint256 _botId, uint256 _price) view internal returns (bool upkeepNeeded, uint256 breachIndex, bool isFirstBreach) {
         uint256 minDistance = type(uint256).max;
-        uint256 breachIndex = grids.length;
+        breachIndex = bots[_botId].grids.length;
+        uint256 lastExecutionTime = bots[_botId].lastExecutionTime;
+        uint256[] memory grids = bots[_botId].grids;
         for (uint256 i = 0; i < grids.length; i++) {
-            uint256 distance = price > grids[i] ? price - grids[i] : grids[i] - price;
+            uint256 distance = _price > grids[i] ? _price - grids[i] : grids[i] - _price;
             if (distance < minDistance) {
                 minDistance = distance;
                 breachIndex = i;
-            }
+            } else { 
+                break;
+            } 
         }
         bool telapsed = (block.timestamp - lastExecutionTime) > interval;
+        isFirstBreach = false;
+        upkeepNeeded = false;
         if (minDistance <= 2 && telapsed) { // In vicinity of 0.10% of the Grid
-            if (!breachedGrids[breachIndex]) {
+            if (!bots[_botId].breachedGrids[breachIndex]) {
                 upkeepNeeded = true;
-                performData = abi.encode(breachIndex, true);
+                isFirstBreach = true;
             } else {
                 upkeepNeeded = true;
-                performData = abi.encode(breachIndex, false);
+                isFirstBreach = false;
             }
         }
+        return (upkeepNeeded, breachIndex, isFirstBreach);
     }
 
     function performUpkeep(bytes calldata performData) external override {
-        uint256 amountToSell = 0;
-        (uint256 breachedIndex, bool isFirstBreach) = abi.decode(performData, (uint256, bool));
-        if (isFirstBreach) { // This needs to fixed maybe add same sell code in else part 
-            breachedGrids[breachedIndex] = true;
-            breachCounter++; 
-            // Perform Buy if there is no Order placed in n-1 grid , else Sell
-            if (breachedIndex > 0 && !breachedGrids[breachedIndex - 1]) {
-                buyCounter++;
-                uint256 qty = swapExactInputSingle(150000000000000000, WMATIC, WETH);
-                boughtAmounts[breachedIndex] = qty; // This quantity will come from exactInputSingle() call
-                emit GridBreached(breachedIndex, true , qty, getScaledPrice());
+        // Check gas that this performUpKeep can consume to not to consume more than the gas limit
+        // in that case the logic will be the same but we could have some proxy and manage the bots using
+        // groups or batches 
+        PerformData[] memory performDataUnencoded = abi.decode(performData, (PerformData[]));
+
+        for (uint256 i; i < performDataUnencoded.length; i++) {
+            PerformData memory performDataIndividual = performDataUnencoded[i];
+            Bot storage bot = bots[performDataIndividual.botId];
+            User storage user = userData[bot.owner];
+            bot.isFirstBreach = performDataIndividual.isFirstBreach;
+            uint256 breachIndex = performDataIndividual.breachIndex;
+            if (performDataIndividual.isFirstBreach) { // This needs to fixed maybe add same sell code in else part 
+                bot.breachedGrids[breachIndex] = true;
+                bot.breachCounter++; 
+                // Perform Buy if there is no Order placed in n-1 grid , else Sell
+                if (breachIndex > 0 && !bot.breachedGrids[breachIndex - 1]) {
+                    _buyOrder(breachIndex, bot, user);
+                } else {
+                    _sellOrder(breachIndex, bot, user);
+                }
             } else {
-                amountToSell = boughtAmounts[breachedIndex - 1];
-                if (amountToSell > 0) {
-                    delete boughtAmounts[breachedIndex - 1];
-                    delete breachedGrids[breachedIndex - 1];
-                    sellCounter++;
-                    swapExactInputSingle(amountToSell, WETH, WMATIC);
-                    emit GridBreached(breachedIndex, false, amountToSell, getScaledPrice());
+                if (bot.breachedGrids[breachIndex  - 1]){
+                    _sellOrder(breachIndex, bot, user);
                 }
             }
-        } else {
-            if (breachedGrids[breachedIndex - 1]){
-                amountToSell = boughtAmounts[breachedIndex - 1];
-                if (amountToSell > 0) {
-                    delete boughtAmounts[breachedIndex - 1];
-                    delete breachedGrids[breachedIndex - 1];
-                    sellCounter++;
-                    swapExactInputSingle(amountToSell, WETH, WMATIC);
-                    emit GridBreached(breachedIndex, false, amountToSell, getScaledPrice());
-                }
-            }
-       }
-       lastExecutionTime = block.timestamp;
+            bot.lastExecutionTime = block.timestamp;
+        }
+    }
+
+    function _buyOrder(uint256 _breachedIndex, Bot storage _bot, User storage _user) internal {
+        uint256 availableBalance = _user.balanceWMATIC;
+        _bot.buyCounter++;
+        uint256 qty = swapExactInputSingle(availableBalance, WMATIC, WETH);
+        _bot.boughtAmounts[_breachedIndex] = qty; // This quantity will come from exactInputSingle() call
+        _user.balanceWMATIC -= availableBalance;
+        _user.balanceWETH += qty;
+        emit GridBreached(_breachedIndex, true , qty, getScaledPrice());
+    }
+
+    function _sellOrder(uint256 _breachedIndex, Bot storage _bot, User memory _user) internal {
+        uint256 amountToSell = _bot.boughtAmounts[_breachedIndex - 1];
+        if (amountToSell > 0) {
+            delete _bot.boughtAmounts[_breachedIndex - 1];
+            delete _bot.breachedGrids[_breachedIndex - 1];
+            _bot.sellCounter++;
+            uint256 qty = swapExactInputSingle(amountToSell, WETH, WMATIC);
+            _user.balanceWETH -= amountToSell;
+            _user.balanceWMATIC += qty;
+            emit GridBreached(_breachedIndex, false, amountToSell, getScaledPrice());
+        }
     }
 
 
@@ -148,20 +222,33 @@ contract SingleSwap is AutomationCompatibleInterface {
        }
     }
 
-    function withdraw() public  {
-        address payable to = payable(msg.sender);
-        uint256 balance = address(this).balance;
-        to.transfer(balance);
-    }
-
-     function depositWmatic(uint256 amount) public payable {
+    function depositWmatic(uint256 amount) public payable {
         require(msg.value == 0, "ETH not accepted");
+        User storage user = userData[msg.sender];
         IERC20(WMATIC).transferFrom(msg.sender, address(this), amount);
+        // check if sender exists, if not create and assign balance
+        if (user.onboarded) {
+            user.balanceWMATIC += amount;
+        } else {
+            user.onboarded = true;
+            user.balanceWMATIC = amount;
+        }
     }
     
     function withdrawWmatic(uint256 amount) public {
+        User storage user = userData[msg.sender];
+        require(user.onboarded && user.balanceWMATIC >= amount, "Insufficient balance");
         require(IERC20(WMATIC).balanceOf(address(this)) >= amount, "Insufficient balance");        
         IERC20(WMATIC).transfer(msg.sender, amount);
+        user.balanceWMATIC -= amount;
+    }
+
+    function withdrawWeth(uint256 amount) public {
+        User storage user = userData[msg.sender];
+        require(user.onboarded && user.balanceWETH >= amount, "Insufficient balance");
+        require(IERC20(WMATIC).balanceOf(address(this)) >= amount, "Insufficient balance");        
+        IERC20(WMATIC).transfer(msg.sender, amount);
+        user.balanceWETH -= amount;
     }
 
 

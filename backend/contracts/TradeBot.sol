d904b62ef396a96728dede2ded373370e738c4a8
pablo_cosio <pablo.cosio@i2cat.net>
2023-06-06 22:41:54 +0200

    Apply some fixes and add data to event logs

--- backend/contracts/TradeBot.sol
+++ backend/contracts/TradeBot.sol
@@ -23,6 +23,10 @@ contract SingleSwap is AutomationCompatibleInterface {
 
     AggregatorV3Interface internal priceFeed;
     address public constant routerAddress = 0xE592427A0AEce92De3Edee1F18E0157C05861564;
+    uint256 public constant minimunAmountAllowed = 1500000000000000;
+    // ToDo
+    uint256 public constant minToDelete = 1500000000000000;
+
     ISwapRouter public immutable swapRouter = ISwapRouter(routerAddress);
 
     uint256 public constant balanceToSpentBPS = 2500; // 25% of the balance will be used to make the swappings
@@ -61,7 +65,7 @@ contract SingleSwap is AutomationCompatibleInterface {
     // Bot ID => token address => amount
     mapping(uint256 => mapping (address => uint256)) public balances;
 
-    event OrderExecuted(OrderType ordertype, uint256 gridIndex, uint256 qty , uint256 price);
+    event OrderExecuted(address user, uint256 botId, OrderType ordertype, uint256 gridIndex, uint256 qty , uint256 price, uint256 timestamp);
 
     // For this example, we will set the pool fee to 0.3%.
     uint24 public constant poolFee = 3000;
@@ -70,7 +74,7 @@ contract SingleSwap is AutomationCompatibleInterface {
         uint256 currentPrice = getScaledPrice();
         require(currentPrice >= _lower_range, "current price should be greater than lower range");
         IERC20(tokenIn).transferFrom(msg.sender, address(this), _amount);
-        balances[botCounter + 1][tokenIn] += _amount;
+        balances[botCounter][tokenIn] += _amount;
         uint256[] memory grids;
         Bot memory newBot = Bot({
             user: msg.sender,
@@ -95,7 +99,7 @@ contract SingleSwap is AutomationCompatibleInterface {
 
         bots[botCounter].currentGrid = calculateGrid(bots[botCounter].grids, currentPrice);
 
-        botCounter ++;
+        botCounter++;
     }
 
     function getGrids(uint256 _botIndex) public view returns (uint256[] memory _grids)  {
@@ -117,12 +121,12 @@ contract SingleSwap is AutomationCompatibleInterface {
         }
     }
 
-    function checkUpkeep(bytes calldata /*checkData*/) external view override returns (bool upkeepNeeded, bytes memory performData)
+    function checkUpkeep(bytes calldata /* checkData */) external view override returns (bool upkeepNeeded, bytes memory performData)
     {
         PerformData[] memory performDataUnencoded = new PerformData[](botCounter); // for each botId related the PerfomData
         upkeepNeeded = false;
 
-        /*(uint256 price) = abi.decode(
+        /* (uint256 price) = abi.decode(
             checkData,
             (uint256)
         );*/
@@ -149,8 +153,8 @@ contract SingleSwap is AutomationCompatibleInterface {
     }
 
     function performUpkeep(bytes calldata performData) external override {
-        // uint256 price = getScaledPrice();
-        uint256 price = 1500;
+        uint256 price = getScaledPrice();
+        // uint256 price = 1500;
         PerformData[] memory performDataDecoded = abi.decode(performData, (PerformData[]));
         for (uint256 i = 0; i < performDataDecoded.length; i++) {
             PerformData memory performDataIndividual = performDataDecoded[i];
@@ -160,28 +164,35 @@ contract SingleSwap is AutomationCompatibleInterface {
             OrderType orderType = performDataIndividual.orderType;
             Bot storage bot = bots[botId];
             if(orderType == OrderType.BuyOrder) {
+                // ToDo => if price goes down multiple grids, multiple buys need to be placed
                 // BUY
                 uint256 amountToSwap = (balances[i][bot.tokenIn] * balanceToSpentBPS) / 10000;
-                uint256 qty = swapExactInputSingle(amountToSwap, bot.tokenIn, bot.tokenOut, true);
-                balances[i][bot.tokenIn] -= amountToSwap;
-                balances[i][bot.tokenOut] += qty;
+                // autocancel bot
+                if (amountToSwap < minimunAmountAllowed) {
+                    bot.isCancelled = true;
+                    continue;
+                }
+                uint256 qty = swapExactInputSingle(amountToSwap, bot.tokenIn, bot.tokenOut);
+                balances[botId][bot.tokenIn] -= amountToSwap;
+                balances[botId][bot.tokenOut] += qty;
                 bot.buyCounter++;
                 breachedBotGrids[botId][breachIndex] = true;
                 boughtBotAmounts[botId][breachIndex] = qty; // store WETH that bot has gathered as a profit swap
                 bot.currentGrid = breachIndex;
-                emit OrderExecuted(orderType, breachIndex, qty, price);
+                emit OrderExecuted(bot.user, botId, orderType, breachIndex, qty, price, block.timestamp);
             }
             if(orderType == OrderType.SellOrder) {
+                // ToDo => if price goes down multiple grids, multiple sells need to be placed
                 // SELL
                 uint256 amountToSwap = boughtBotAmounts[botId][breachIndex - 2];
-                uint256 qty = swapExactInputSingle(amountToSwap, bot.tokenIn, bot.tokenOut, false);
-                balances[i][bot.tokenIn] += amountToSwap;
-                balances[i][bot.tokenOut] -= qty;
+                uint256 qty = swapExactInputSingle(amountToSwap, bot.tokenOut, bot.tokenIn);
+                balances[botId][bot.tokenIn] += qty;
+                balances[botId][bot.tokenOut] -= amountToSwap;
                 bot.sellCounter++;
                 delete breachedBotGrids[botId][breachIndex - 2];
                 delete boughtBotAmounts[botId][breachIndex - 2];
                 bot.currentGrid = breachIndex;
-                emit OrderExecuted(orderType, breachIndex, qty, price);
+                emit OrderExecuted(bot.user, botId, orderType, breachIndex, qty, price, block.timestamp);
             }
             if (orderType == OrderType.UpdateCurrentGrid) {
                 bot.currentGrid = breachIndex;
@@ -191,13 +202,14 @@ contract SingleSwap is AutomationCompatibleInterface {
     }
 
 
-    function swapExactInputSingle(uint256 amountIn, address tin , address tout, bool isBuyOrder) public payable returns (uint256 amountOut)
+    function swapExactInputSingle(uint256 amountIn, address tin , address tout) public payable returns (uint256 amountOut)
     {
-        if (isBuyOrder) {
+        /* if (isBuyOrder) {
             IERC20(tin).approve(address(swapRouter), amountIn);
         } else {
             IERC20(tout).approve(address(swapRouter), amountIn);
-        }
+        }*/
+        IERC20(tin).approve(address(swapRouter), amountIn);
 
         ISwapRouter.ExactInputSingleParams memory params = ISwapRouter
             .ExactInputSingleParams({
@@ -252,7 +264,15 @@ contract SingleSwap is AutomationCompatibleInterface {
 
     function deleteBot(uint256 botIndex) external {
         require(bots[botIndex].user == msg.sender, "Only user can toogle");
+        require(balances[botIndex][bots[botIndex].tokenIn] > minToDelete, "Balance should be 0");
+        require(balances[botIndex][bots[botIndex].tokenOut] > minToDelete, "Balance should be 0");
         delete bots[botIndex];
+    }
+
+    function deposit(uint256 _amount, uint256 _botId) external {
+        Bot memory bot = bots[_botId];
+        IERC20(bot.tokenIn).transferFrom(msg.sender, address(this), _amount);
+        balances[_botId][bot.tokenIn] += _amount;
     }
     
 }
